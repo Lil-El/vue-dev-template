8919ede9408c44a9706337c6b322b694f77d9ef7
unknown <yxd99324@qq.com>
2020-07-29 11:33:07 +0800

    split el-ui

--- src/main.js
+++ src/main.js
@@ -3,47 +3,13 @@ import VueLazyload from "vue-lazyload";
 
 import "normalize.css/normalize.css"; // a modern alternative to CSS resets
 
-import {
-  Button,
-  DropdownMenu,
-  DropdownItem,
-  Dropdown,
-  Breadcrumb,
-  BreadcrumbItem,
-  Badge,
-  Dialog,
-  Table,
-  Tag,
-  TableColumn,
-  Upload,
-  Menu,
-  MenuItem,
-  Submenu,
-  Row,
-  Col,
-  Card,
-  Progress,
-  Tabs,
-  TabPane,
-  Form,
-  FormItem,
-  Carousel,
-  Timeline,
-  TimelineItem,
-  CarouselItem,
-  Popconfirm,
-  Input,
-  Tree,
-  RadioGroup,
-  RadioButton
-} from "element-ui";
 import "./styles/element-variables.scss";
 import "@/styles/index.scss";
 
 import App from "./App";
 import store from "./store";
 import router from "./router";
-
+import "./loadElUI";
 // import "./icons";
 import "./permission";
 import "./utils/error-log";
@@ -59,42 +25,7 @@ Object.keys(filters).forEach(key => {
 });
 Vue.config.productionTip = false;
 
-//#region
-Vue.use(Button);
-Vue.use(DropdownMenu);
-Vue.use(DropdownItem);
-Vue.use(Dropdown);
-Vue.use(Breadcrumb);
-Vue.use(BreadcrumbItem);
-Vue.use(Badge);
-Vue.use(Dialog);
-Vue.use(Table);
-Vue.use(Tag);
-Vue.use(TableColumn);
-Vue.use(Upload);
-Vue.use(Menu);
-Vue.use(MenuItem);
-Vue.use(Submenu);
-Vue.use(Row);
-Vue.use(Col);
-Vue.use(Card);
-Vue.use(Progress);
-Vue.use(Tabs);
-Vue.use(TabPane);
-Vue.use(Form);
-Vue.use(FormItem);
-Vue.use(Carousel);
-Vue.use(Timeline);
-Vue.use(TimelineItem);
-Vue.use(CarouselItem);
-Vue.use(Popconfirm);
-Vue.use(Input);
-Vue.use(Tree);
-Vue.use(RadioGroup);
-Vue.use(RadioButton);
 Vue.use(VueLazyload);
-//#endregion
-
 new Vue({
   el: "#app",
   router,
